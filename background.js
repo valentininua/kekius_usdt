2af31992fc1ecad4d3490fe85636ec6ddc668754
Valentin Badiul S <380961726999@ya.ru>
2025-01-08 21:54:02 +0200

    0.073668

---
+++ background.js
@@ -0,0 +1,25 @@
+chrome.runtime.onInstalled.addListener(() => {
+    chrome.alarms.create("fetchGateData", { periodInMinutes: 1 }); //updates the price every minute
+  });
+  
+  chrome.alarms.onAlarm.addListener((alarm) => {
+    if (alarm.name === "fetchGateData") {
+      fetchKEKIUSUSDT();
+    }
+  });
+
+  async function fetchKEKIUSUSDT() {
+    const url = "https://api.gateio.ws/api/v4/spot/tickers?currency_pair=KEKIUS_USDT";
+    try {
+      const response = await fetch(url);
+      const data = await response.json();
+      const price = data[0]?.last || "Не найдено";
+      console.log(`Курс KEKIUS/USDT: $${parseFloat(price).toFixed(6)}`);
+      // Устанавливаем текст на значок
+      chrome.action.setBadgeText({ text: parseFloat(price).toFixed(2) });
+      chrome.action.setBadgeBackgroundColor({ color: "#4CAF50" }); // Зеленый фон
+    } catch (error) {
+      console.error("Ошибка получения данных:", error);
+    }
+  }
+  
---
+++ popup.js
@@ -0,0 +1,32 @@
+async function fetchKEKIUSUSDT() {
+  const url = "https://api.gateio.ws/api/v4/spot/tickers?currency_pair=KEKIUS_USDT";
+  const urlCoin = "https://valentin.in.ua/coin.json";
+  try {
+    const response = await fetch(url);
+    const data = await response.json(); 
+    const responseCoin = await fetch(urlCoin);
+    const dataCoin = await responseCoin.json();
+    const myKekius = await parseFloat(dataCoin.KEKIUS_USDT);
+
+    //возвращает массив объектов, выбираем первый
+    const price = data[0]?.last || "Не найдено";
+    const high24h = data[0]?.high_24h || "Не найдено";
+    const low24h = data[0]?.low_24h || "Не найдено";
+
+    document.getElementById("rate").innerText = `${parseFloat(price).toFixed(6)}`;
+    document.getElementById("high24h").innerText = `${parseFloat(high24h).toFixed(6)}`;
+    document.getElementById("low24h").innerText = `${parseFloat(low24h).toFixed(6)}`;
+
+    document.getElementById("myUsdt").innerText = parseFloat(price).toFixed(6) * myKekius;
+    document.getElementById("coin").innerText = myKekius;
+  } catch (error) {
+    document.getElementById("rate").innerText = "Ошибка загрузки!";
+    console.error("Ошибка получения курса:", error);
+  }
+}
+
+// Обновляем курс при загрузке popup
+fetchKEKIUSUSDT();
+
+// Обновление каждые 10 секунд (опционально)
+setInterval(fetchKEKIUSUSDT, 10000);
